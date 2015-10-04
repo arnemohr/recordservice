ibmsoe/ImpalaPPC

IMPALA-2079: Part 2: handling tmp device errors
Tmp devices are blacklisted when a write error is encountered for that
device. No more scratch space will be allocated on the blacklisted
device, based on the assumption that the device is likely to be
misconfigured or failing.

This patch does not attempt to recover the query that experienced the
write error. It also does not attempt to remap any existing blocks away
from the temporary device.

This behaviour is unit tested for several failure scenarios.

This patch adds additional test infrastructure required for testing
BufferedBlockMgr behavior in the presence of faults and in
configurations with multiple tmp directories.

Adds metrics tmp-file-mgr.active-scratch-dirs and
tmp-file-mgr.active-scratch-dirs.list that track the number and set of
active scratch dirs and expose it in the Impala web UI.

Change-Id: I9d80ed3a7afad6ff8e5d739b6ea2bc0949f16746
Reviewed-on: http://gerrit.cloudera.org:8080/579
Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
Tested-by: Internal Jenkins

be/src/runtime/tmp-file-mgr.h
@@ -15,19 +15,27 @@
 #ifndef IMPALA_RUNTIME_TMP_FILE_MGR_H
 #define IMPALA_RUNTIME_TMP_FILE_MGR_H
 
-#include <common/status.h>
+#include "common/status.h"
 #include "gen-cpp/Types_types.h"  // for TUniqueId
+#include "util/collection-metrics.h"
+#include "util/spinlock.h"
 
 namespace impala {
 
-/// TmpFileMgr is a utility class that creates temporary directories on the local
-/// filesystem.
-//
-/// TmpFileMgr::GetFile() is used to return a TmpFileMgr::File handle with a unique
-/// filename on a specified temp file device - the client owns the handle.
+/// TmpFileMgr creates and manages temporary files and directories on the local
+/// filesystem. It can manage multiple temporary directories across multiple devices.
+/// TmpFileMgr ensures that at most one directory per device is used unless overridden
+/// for testing. GetFile() returns a File handle with a unique filename on a device. The
+/// client owns the File handle and can use it to expand the file.
+/// TODO: we could notify block managers about the failure so they can more take
+/// proactive action to avoid using the device.
 class TmpFileMgr {
  public:
-  /// TmpFileMgr::File is a handle to a physical file in a temporary directory. Clients
+  /// DeviceId is a unique identifier for a temporary device managed by TmpFileMgr.
+  /// It is used as a handle for external classes to identify devices.
+  typedef int DeviceId;
+
+  /// File is a handle to a physical file in a temporary directory. Clients
   /// can allocate file space and remove files using AllocateSpace() and Remove().
   /// Creation of the file is deferred until the first call to AllocateSpace().
   class File {
@@ -35,7 +43,13 @@ class TmpFileMgr {
     /// Allocates 'write_size' bytes in this file for a new block of data.
     /// The file size is increased by a call to truncate() if necessary.
     /// The physical file is created on the first call to AllocateSpace().
+    /// Returns Status::OK() and sets offset on success.
+    /// Returns an error status if an unexpected error occurs.
+    /// If an error status is returned, the caller can try a different temporary file.
     Status AllocateSpace(int64_t write_size, int64_t* offset);
+
+    /// Called to notify TmpFileMgr that an IO error was encountered for this file
+    void ReportIOError(const ErrorMsg& msg);
 
     /// Delete the physical file on disk, if one was created.
     /// It is not valid to read or write to a file after calling Remove().
@@ -43,6 +57,7 @@ class TmpFileMgr {
 
     const std::string& path() const { return path_; }
     int disk_id() const { return disk_id_; }
+    bool is_blacklisted() const { return blacklisted_; }
 
    private:
     friend class TmpFileMgr;
@@ -55,38 +70,103 @@ class TmpFileMgr {
     /// directory. A warning is issued if available space is less than this threshold.
     const static uint64_t AVAILABLE_SPACE_THRESHOLD_MB;
 
-    File(const std::string& path);
+    File(TmpFileMgr* mgr, DeviceId device_id, const std::string& path);
+
+    /// TmpFileMgr this belongs to.
+    TmpFileMgr* mgr_;
 
     /// Path of the physical file in the filesystem.
     std::string path_;
+
+    /// The temporary device this file is stored on.
+    DeviceId device_id_;
 
     /// The id of the disk on which the physical file lies.
     int disk_id_;
 
     /// Current file size. Modified by AllocateSpace(). Size is 0 before file creation.
     int64_t current_size_;
+
+    /// Set to true to indicate that file can't be expanded. This is useful to keep here
+    /// even though it is redundant with the global per-device blacklisting in TmpFileMgr
+    /// because it can be checked without acquiring a global lock. If a file is
+    /// blacklisted, the corresponding device will always be blacklisted.
+    bool blacklisted_;
   };
 
-  /// Creates the tmp directories configured by CM. If multiple directories are specified
-  /// per disk, only one is created and used. Must be called after DiskInfo::Init().
-  static Status Init();
+  TmpFileMgr();
+
+  /// Creates the configured tmp directories. If multiple directories are specified per
+  /// disk, only one is created and used. Must be called after DiskInfo::Init().
+  Status Init(MetricGroup* metrics);
+
+  /// Custom initialization - initializes with the provided list of directories.
+  /// If one_dir_per_device is true, only use one temporary directory per device.
+  /// This interface is intended for testing purposes.
+  Status InitCustom(const std::vector<std::string>& tmp_dirs, bool one_dir_per_device,
+      MetricGroup* metrics);
 
   /// Return a new File handle with a unique path for a query instance. The file path
   /// is within the (single) tmp directory on the specified device id. The caller owns
   /// the returned handle and is responsible for deleting it. The file is not created -
   /// creation is deferred until the first call to File::AllocateSpace().
-  static Status GetFile(int tmp_device_id, const TUniqueId& query_id,
+  Status GetFile(const DeviceId& device_id, const TUniqueId& query_id,
       File** new_file);
 
-  /// Total number of devices with tmp directories. This is the same as the number
-  /// of tmp directories created.
-  static int num_tmp_devices() { return tmp_dirs_.size(); }
+  /// Return the scratch directory path for the device.
+  std::string GetTmpDirPath(DeviceId device_id) const;
+
+  /// Total number of devices with tmp directories that are active. There is one tmp
+  /// directory per device.
+  int num_active_tmp_devices();
+
+  /// Return vector with device ids of all tmp devices being actively used.
+  /// I.e. those that haven't been blacklisted.
+  std::vector<DeviceId> active_tmp_devices();
 
  private:
-  static bool initialized_;
+  /// Dir stores information about a temporary directory.
+  class Dir {
+   public:
+    const std::string& path() const { return path_; }
 
-  /// The created tmp directories, atmost one per device.
-  static std::vector<std::string> tmp_dirs_;
+    // Return true if it was newly added to blacklist.
+    bool blacklist() {
+      bool was_blacklisted = blacklisted_;
+      blacklisted_ = true;
+      return !was_blacklisted;
+    }
+    bool is_blacklisted() const { return blacklisted_; }
+
+   private:
+    friend class TmpFileMgr;
+
+    /// path should be a absolute path to a writable scratch directory.
+    Dir(const std::string& path, bool blacklisted)
+        : path_(path), blacklisted_(blacklisted) {}
+
+    std::string path_;
+
+    bool blacklisted_;
+  };
+
+  /// Remove a device from the rotation. Subsequent attempts to allocate a file on that
+  /// device will fail and the device will not be included in active tmp devices.
+  void BlacklistDevice(DeviceId device_id);
+
+  bool IsBlacklisted(DeviceId device_id);
+
+  bool initialized_;
+
+  /// Protects the status of tmp dirs (i.e. whether they're blacklisted).
+  SpinLock dir_status_lock_;
+
+  /// The created tmp directories.
+  std::vector<Dir> tmp_dirs_;
+
+  /// Metrics to track active scratch directories.
+  IntGauge* num_active_scratch_dirs_metric_;
+  SetMetric<std::string>* active_scratch_dirs_metric_;
 };
 
 }

arnemohr/recordservice

be/src/runtime/tmp-file-mgr.h
@@ -15,19 +15,27 @@
 #ifndef IMPALA_RUNTIME_TMP_FILE_MGR_H
 #define IMPALA_RUNTIME_TMP_FILE_MGR_H
 
-#include <common/status.h>
+#include "common/status.h"
 #include "gen-cpp/Types_types.h"  // for TUniqueId
+#include "util/collection-metrics.h"
+#include "util/locks.h"
 
 namespace impala {
 
-/// TmpFileMgr is a utility class that creates temporary directories on the local
-/// filesystem.
-//
-/// TmpFileMgr::GetFile() is used to return a TmpFileMgr::File handle with a unique
-/// filename on a specified temp file device - the client owns the handle.
+/// TmpFileMgr creates and manages temporary files and directories on the local
+/// filesystem. It can manage multiple temporary directories across multiple devices.
+/// TmpFileMgr ensures that at most one directory per device is used unless overridden
+/// for testing. GetFile() returns a File handle with a unique filename on a device. The
+/// client owns the File handle and can use it to expand the file.
+/// TODO: we could notify block managers about the failure so they can more take
+/// proactive action to avoid using the device.
 class TmpFileMgr {
  public:
-  /// TmpFileMgr::File is a handle to a physical file in a temporary directory. Clients
+  /// DeviceId is a unique identifier for a temporary device managed by TmpFileMgr.
+  /// It is used as a handle for external classes to identify devices.
+  typedef int DeviceId;
+
+  /// File is a handle to a physical file in a temporary directory. Clients
   /// can allocate file space and remove files using AllocateSpace() and Remove().
   /// Creation of the file is deferred until the first call to AllocateSpace().
   class File {
@@ -35,7 +43,13 @@ class TmpFileMgr {
     /// Allocates 'write_size' bytes in this file for a new block of data.
     /// The file size is increased by a call to truncate() if necessary.
     /// The physical file is created on the first call to AllocateSpace().
+    /// Returns Status::OK() and sets offset on success.
+    /// Returns an error status if an unexpected error occurs.
+    /// If an error status is returned, the caller can try a different temporary file.
     Status AllocateSpace(int64_t write_size, int64_t* offset);
+
+    /// Called to notify TmpFileMgr that an IO error was encountered for this file
+    void ReportIOError(const ErrorMsg& msg);
 
     /// Delete the physical file on disk, if one was created.
     /// It is not valid to read or write to a file after calling Remove().
@@ -43,6 +57,7 @@ class TmpFileMgr {
 
     const std::string& path() const { return path_; }
     int disk_id() const { return disk_id_; }
+    bool is_blacklisted() const { return blacklisted_; }
 
    private:
     friend class TmpFileMgr;
@@ -55,38 +70,103 @@ class TmpFileMgr {
     /// directory. A warning is issued if available space is less than this threshold.
     const static uint64_t AVAILABLE_SPACE_THRESHOLD_MB;
 
-    File(const std::string& path);
+    File(TmpFileMgr* mgr, DeviceId device_id, const std::string& path);
+
+    /// TmpFileMgr this belongs to.
+    TmpFileMgr* mgr_;
 
     /// Path of the physical file in the filesystem.
     std::string path_;
+
+    /// The temporary device this file is stored on.
+    DeviceId device_id_;
 
     /// The id of the disk on which the physical file lies.
     int disk_id_;
 
     /// Current file size. Modified by AllocateSpace(). Size is 0 before file creation.
     int64_t current_size_;
+
+    /// Set to true to indicate that file can't be expanded. This is useful to keep here
+    /// even though it is redundant with the global per-device blacklisting in TmpFileMgr
+    /// because it can be checked without acquiring a global lock. If a file is
+    /// blacklisted, the corresponding device will always be blacklisted.
+    bool blacklisted_;
   };
 
-  /// Creates the tmp directories configured by CM. If multiple directories are specified
-  /// per disk, only one is created and used. Must be called after DiskInfo::Init().
-  static Status Init();
+  TmpFileMgr();
+
+  /// Creates the configured tmp directories. If multiple directories are specified per
+  /// disk, only one is created and used. Must be called after DiskInfo::Init().
+  Status Init(MetricGroup* metrics);
+
+  /// Custom initialization - initializes with the provided list of directories.
+  /// If one_dir_per_device is true, only use one temporary directory per device.
+  /// This interface is intended for testing purposes.
+  Status InitCustom(const std::vector<std::string>& tmp_dirs, bool one_dir_per_device,
+      MetricGroup* metrics);
 
   /// Return a new File handle with a unique path for a query instance. The file path
   /// is within the (single) tmp directory on the specified device id. The caller owns
   /// the returned handle and is responsible for deleting it. The file is not created -
   /// creation is deferred until the first call to File::AllocateSpace().
-  static Status GetFile(int tmp_device_id, const TUniqueId& query_id,
+  Status GetFile(const DeviceId& device_id, const TUniqueId& query_id,
       File** new_file);
 
-  /// Total number of devices with tmp directories. This is the same as the number
-  /// of tmp directories created.
-  static int num_tmp_devices() { return tmp_dirs_.size(); }
+  /// Return the scratch directory path for the device.
+  std::string GetTmpDirPath(DeviceId device_id) const;
+
+  /// Total number of devices with tmp directories that are active. There is one tmp
+  /// directory per device.
+  int num_active_tmp_devices();
+
+  /// Return vector with device ids of all tmp devices being actively used.
+  /// I.e. those that haven't been blacklisted.
+  std::vector<DeviceId> active_tmp_devices();
 
  private:
-  static bool initialized_;
+  /// Dir stores information about a temporary directory.
+  class Dir {
+   public:
+    const std::string& path() const { return path_; }
 
-  /// The created tmp directories, atmost one per device.
-  static std::vector<std::string> tmp_dirs_;
+    // Return true if it was newly added to blacklist.
+    bool blacklist() {
+      bool was_blacklisted = blacklisted_;
+      blacklisted_ = true;
+      return !was_blacklisted;
+    }
+    bool is_blacklisted() const { return blacklisted_; }
+
+   private:
+    friend class TmpFileMgr;
+
+    /// path should be a absolute path to a writable scratch directory.
+    Dir(const std::string& path, bool blacklisted)
+        : path_(path), blacklisted_(blacklisted) {}
+
+    std::string path_;
+
+    bool blacklisted_;
+  };
+
+  /// Remove a device from the rotation. Subsequent attempts to allocate a file on that
+  /// device will fail and the device will not be included in active tmp devices.
+  void BlacklistDevice(DeviceId device_id);
+
+  bool IsBlacklisted(DeviceId device_id);
+
+  bool initialized_;
+
+  /// Protects the status of tmp dirs (i.e. whether they're blacklisted).
+  SpinLock dir_status_lock_;
+
+  /// The created tmp directories.
+  std::vector<Dir> tmp_dirs_;
+
+  /// Metrics to track active scratch directories.
+  IntGauge* num_active_scratch_dirs_metric_;
+  SetMetric<std::string>* active_scratch_dirs_metric_;
 };
 
 }