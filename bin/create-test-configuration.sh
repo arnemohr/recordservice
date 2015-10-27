ed5fd6542555c571665303f59890103c2b28788b
lili <lili@cloudera.com>
2015-10-27 11:10:56 -0700

    set CREATE_SENTRY_STORE=1 by default as jira: SENTRY-935
    Change-Id: I612d0aca246090880d49136fe5aeb60279a33750
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/9054
    Tested-by: jenkins
    Reviewed-by: Chao Sun <chao.sun@cloudera.com>

--- bin/create-test-configuration.sh
+++ bin/create-test-configuration.sh
@@ -19,7 +19,8 @@
 
 set -e
 CREATE_METASTORE=0
-CREATE_SENTRY_STORE=0
+# Set CREATE_SENTRY_STORE=1 by default, as jira: SENTRY-935
+CREATE_SENTRY_STORE=1
 
 # parse command line options
 for ARG in $*
