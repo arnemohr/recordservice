cloudera/Impala

IMPALA-2310: Add PURGE option to DROP TABLE/ALTER TBL DROP PART
This commit adds PURGE option to DROP TABLE/ALTER TABLE DROP
PARTITION statements. Following is the usage:

1. DROP TABLE <tablename> takes an optional argument PURGE. Adding
purge purges the table data by skipping trash, if configured.

  DROP TABLE [<database>.]<tablename> [IF EXISTS] [PURGE]

2. PURGE is also supported with alter table drop partition query
with the following syntax. If specified, impala purges the partition
data by skipping trash.

  ALTER TABLE [<database>.]<tablename> DROP PARTITION [IF EXISTS] [PURGE]

This patch also helps the use case where Trash and the data directories
are in different encryption zones, in which case we cannot move the data
during ALTER/DROP. Then purge option can be used to skip the trash and
make sure data is actually deleted.

Change-Id: I64bf71d660b719896c32e0f3a7ab768f30ec7b3b
(cherry picked from commit 585d4f8d9e809f3bf194018dd161a22d3f144270)
Reviewed-on: http://gerrit.cloudera.org:8080/1244
Reviewed-by: Juan Yu <jyu@cloudera.com>
Tested-by: Internal Jenkins

fe/src/main/cup/sql-parser.y
@@ -245,8 +245,8 @@ terminal
   KW_LOAD, KW_LOCATION, KW_MAP, KW_MERGE_FN, KW_METADATA, KW_NOT, KW_NULL, KW_NULLS,
   KW_OFFSET, KW_ON, KW_OR, KW_ORDER, KW_OUTER, KW_OVER, KW_OVERWRITE, KW_PARQUET,
   KW_PARQUETFILE, KW_PARTITION, KW_PARTITIONED, KW_PARTITIONS, KW_PRECEDING,
-  KW_PREPARE_FN, KW_PRODUCED, KW_RANGE, KW_RCFILE, KW_RECOVER, KW_REFRESH, KW_REGEXP, KW_RENAME,
-  KW_REPLACE, KW_REPLICATION, KW_RESTRICT, KW_RETURNS,
+  KW_PREPARE_FN, KW_PRODUCED, KW_PURGE, KW_RANGE, KW_RCFILE, KW_RECOVER, KW_REFRESH,
+  KW_REGEXP, KW_RENAME, KW_REPLACE, KW_REPLICATION, KW_RESTRICT, KW_RETURNS,
   KW_REVOKE, KW_RIGHT, KW_RLIKE, KW_ROLE,
   KW_ROLES, KW_ROW, KW_ROWS, KW_SCHEMA, KW_SCHEMAS, KW_SELECT, KW_SEMI, KW_SEQUENCEFILE,
   KW_SERDEPROPERTIES, KW_SERIALIZE_FN, KW_SET, KW_SHOW, KW_SMALLINT, KW_STORED,
@@ -386,6 +386,7 @@ nonterminal HdfsCachingOp cache_op_val;
 nonterminal BigDecimal opt_cache_op_replication;
 nonterminal String comment_val;
 nonterminal Boolean external_val;
+nonterminal Boolean purge_val;
 nonterminal String opt_init_string_val;
 nonterminal THdfsFileFormat file_format_val;
 nonterminal THdfsFileFormat file_format_create_table_val;
@@ -776,8 +777,8 @@ alter_tbl_stmt ::=
     column_def:col_def
   {: RESULT = new AlterTableChangeColStmt(table, col_name, col_def); :}
   | KW_ALTER KW_TABLE table_name:table KW_DROP if_exists_val:if_exists
-    partition_spec:partition
-  {: RESULT = new AlterTableDropPartitionStmt(table, partition, if_exists); :}
+    partition_spec:partition purge_val:purge
+  {: RESULT = new AlterTableDropPartitionStmt(table, partition, if_exists, purge); :}
   | KW_ALTER KW_TABLE table_name:table opt_partition_spec:partition KW_SET KW_FILEFORMAT
     file_format_val:file_format
   {: RESULT = new AlterTableSetFileFormatStmt(table, partition, file_format); :}
@@ -1005,6 +1006,13 @@ opt_init_string_val ::=
 
 external_val ::=
   KW_EXTERNAL
+  {: RESULT = true; :}
+  |
+  {: RESULT = false; :}
+  ;
+
+purge_val ::=
+  KW_PURGE
   {: RESULT = true; :}
   |
   {: RESULT = false; :}
@@ -1261,10 +1269,10 @@ drop_db_stmt ::=
   ;
 
 drop_tbl_or_view_stmt ::=
-  KW_DROP KW_TABLE if_exists_val:if_exists table_name:table
-  {: RESULT = new DropTableOrViewStmt(table, if_exists, true); :}
+  KW_DROP KW_TABLE if_exists_val:if_exists table_name:table purge_val:purge
+  {: RESULT = new DropTableOrViewStmt(table, if_exists, true, purge); :}
   | KW_DROP KW_VIEW if_exists_val:if_exists table_name:table
-  {: RESULT = new DropTableOrViewStmt(table, if_exists, false); :}
+  {: RESULT = new DropTableOrViewStmt(table, if_exists, false, false); :}
   ;
 
 drop_function_stmt ::=

arnemohr/recordservice

(cherry picked from commit a55a0cb67515e62a1d4ef90734ff62081d6a3554)
Conflicts fixed:
	fe/src/main/cup/sql-parser.y

fe/src/main/cup/sql-parser.y
@@ -245,7 +245,7 @@ terminal
   KW_LOAD, KW_LOCATION, KW_MAP, KW_MERGE_FN, KW_METADATA, KW_NOT, KW_NULL, KW_NULLS,
   KW_OFFSET, KW_ON, KW_OR, KW_ORDER, KW_OUTER, KW_OVER, KW_OVERWRITE, KW_PARQUET,
   KW_PARQUETFILE, KW_PARTITION, KW_PARTITIONED, KW_PARTITIONS, KW_PRECEDING,
-  KW_PREPARE_FN, KW_PRODUCED, KW_RANGE, KW_RCFILE, KW_RECORDSERVICE, KW_RECOVER,
+  KW_PREPARE_FN, KW_PRODUCED, KW_PURGE, KW_RANGE, KW_RCFILE, KW_RECORDSERVICE, KW_RECOVER,
   KW_REFRESH, KW_REGEXP, KW_RENAME,
   KW_REPLACE, KW_REPLICATION, KW_RESTRICT, KW_RETURNS,
   KW_REVOKE, KW_RIGHT, KW_RLIKE, KW_ROLE,
@@ -387,6 +387,7 @@ nonterminal HdfsCachingOp cache_op_val;
 nonterminal BigDecimal opt_cache_op_replication;
 nonterminal String comment_val;
 nonterminal Boolean external_val;
+nonterminal Boolean purge_val;
 nonterminal String opt_init_string_val;
 nonterminal THdfsFileFormat file_format_val;
 nonterminal THdfsFileFormat file_format_create_table_val;
@@ -777,8 +778,8 @@ alter_tbl_stmt ::=
     column_def:col_def
   {: RESULT = new AlterTableChangeColStmt(table, col_name, col_def); :}
   | KW_ALTER KW_TABLE table_name:table KW_DROP if_exists_val:if_exists
-    partition_spec:partition
-  {: RESULT = new AlterTableDropPartitionStmt(table, partition, if_exists); :}
+    partition_spec:partition purge_val:purge
+  {: RESULT = new AlterTableDropPartitionStmt(table, partition, if_exists, purge); :}
   | KW_ALTER KW_TABLE table_name:table opt_partition_spec:partition KW_SET KW_FILEFORMAT
     file_format_val:file_format
   {: RESULT = new AlterTableSetFileFormatStmt(table, partition, file_format); :}
@@ -1014,6 +1015,13 @@ opt_init_string_val ::=
 
 external_val ::=
   KW_EXTERNAL
+  {: RESULT = true; :}
+  |
+  {: RESULT = false; :}
+  ;
+
+purge_val ::=
+  KW_PURGE
   {: RESULT = true; :}
   |
   {: RESULT = false; :}
@@ -1275,10 +1283,10 @@ drop_db_stmt ::=
   ;
 
 drop_tbl_or_view_stmt ::=
-  KW_DROP KW_TABLE if_exists_val:if_exists table_name:table
-  {: RESULT = new DropTableOrViewStmt(table, if_exists, true); :}
+  KW_DROP KW_TABLE if_exists_val:if_exists table_name:table purge_val:purge
+  {: RESULT = new DropTableOrViewStmt(table, if_exists, true, purge); :}
   | KW_DROP KW_VIEW if_exists_val:if_exists table_name:table
-  {: RESULT = new DropTableOrViewStmt(table, if_exists, false); :}
+  {: RESULT = new DropTableOrViewStmt(table, if_exists, false, false); :}
   ;
 
 drop_function_stmt ::=